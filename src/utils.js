a65d63b6e5c06e0448473178841445524ed54a92
Alexandr <jacoby3210@gmail.com>
2024-09-02 07:45:42 +0300

    using special links to open files in vscode

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,6 @@
 const BaseURL = 'http://localhost:';
-const middlewareName = '__open-in-editor';
+const port = 9528;
+const middlewareName = 'browser';
 
 // helpers
 export function generateTime() {
@@ -11,32 +12,26 @@ export function generateTime() {
 }
 
 // data
-export function generateLogTitle() {return `"%c[${generateTime()}]`;}
-export function generateFileLocation(location) {return `%c${location} `;}
-export function generateLine(lineCount) {return `%cLine: ${lineCount}\\n`;}
-export function generateAddress(port, filePath) {
-	return `%cJump to: ${BaseURL + port}/${middlewareName}?file=${filePath}\\n`
-}
-export function generateNewLine() {return '"'}
+function generateLogTitle() {return `"%c[${generateTime()}]`;}
+function generateFileLocation(location) {return `%c vscode://file/${location}`;}
+function generateLine(lineCount) {return `%c :${lineCount}\\n`;}
+function generateNewLine() {return '"'}
 
 // style
 function generateLogTitleStyle() 			{return `"color:#3A6F28;padding:2px 5px;font-weight:700;"`}
 function generateFileLocationStyle() 	{return `"color: #00A29B;"`;}
 function generateLineStyle() 					{return `"color: #9E6BB5;"`;}
-function generateAddressStyle() 			{return `"color: #6664C2;"`;}
 
 // style
 export function composeConsoleLog(components) {
-  const { prefix, suffix, fileRelativePath, fileAbsolutePath, lineCount, endColumn, port, jump } = components;
+  const { prefix, suffix, fileRelativePath, fileAbsolutePath, lineCount, endColumn, jump } = components;
   return `${prefix
     + generateLogTitle()
-    + generateFileLocation(fileRelativePath)
+    + generateFileLocation(fileAbsolutePath)
     + generateLine(lineCount)
-    + (jump ? generateAddress(port, encodeURIComponent(`${fileAbsolutePath}:${lineCount}:${endColumn}`)) : '')
     + `${generateNewLine()},`
     + `${generateLogTitleStyle()},`
     + `${generateFileLocationStyle()},`
     + `${generateLineStyle()},`
-    + (jump ? `${generateAddressStyle()},` : '')
     + suffix}`;
 }
